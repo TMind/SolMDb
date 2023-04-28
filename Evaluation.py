8c534d9f8495349328a0152c0b4507d8f977fb45
Stefan R <stefan2581@gmail.com>
2023-04-28 15:02:07 +0200

    Final Evaluation

--- Evaluation.py
+++ Evaluation.py
@@ -1,25 +1,24 @@
 from scipy.stats import hypergeom
 
 
+def calc_prob(s, t, N, n, m, C ):
+    # Calculate probability of drawing at least n target cards and at most m source cards
+    p_target_at_least_n = 1 - hypergeom.cdf(k=n-1, M=N, n=t, N=C)
+    p_source_at_most_m = hypergeom.cdf(k=m, M=N, n=s, N=C)
 
-def calc_probability(t, s, N, c, n, m):
-    # Calculate probability of drawing n out of t cards and m out of s cards
-    pn = hypergeom.cdf(k=n, M=N, n=t, N=c)
-    pm = hypergeom.pmf(k=m, M=N, n=s, N=c)
-    
-    print(f"Drawing at least {n} from {t} in a hand of {c} out of a total {N}")
-    print(f"Drawing exactly  {m} from {s} in a hand of {c} out of a total {N}")
+    # Calculate probability of meeting condition
+    p_meeting_condition = p_target_at_least_n * p_source_at_most_m
 
-    return pn * pm
+    return p_meeting_condition
 
 
 def calc_synergy_validity(s, t):
     # Calculate probability of meeting condition in a single turn
 
 
-    p_target_no_source_first_turn  = calc_probability(s, t, 20, 5, 1, 0)
-    p_target_no_source_second_turn = calc_probability(s, t, 15, 5, 1, 0)
-    p_target_no_source_third_turn  = calc_probability(s, t, 10, 5, 1, 0)
+    p_target_no_source_first_turn  = calc_prob(s, t, 20, 1, 0, 5)
+    p_target_no_source_second_turn = calc_prob(s, t, 15, 1, 0, 5)
+    p_target_no_source_third_turn  = calc_prob(s, t, 10, 1, 0, 5)
 
     p_target_first_turn  = 1 - hypergeom.pmf(k=0, M=20, n=t, N=5)
     p_target_second_turn = 1 - hypergeom.pmf(k=0, M=15, n=t, N=5)
@@ -29,12 +28,12 @@ def calc_synergy_validity(s, t):
                                      (1 - p_target_first_turn) * (1 - p_target_second_turn) *   p_target_no_source_third_turn
     )
 
-    print(f"Target only Turn 1: {p_target_no_source_first_turn}\n "
-          f"Target only Turn 2: {p_target_no_source_second_turn}\n"
-          f"Target only Turn 3: {p_target_no_source_third_turn}\n "
-          f"Target First  Turn: {p_target_first_turn}\n"
-          f"Target Second Turn: {p_target_second_turn}\n"
-          f"Target only 3 Turns: {p_target_no_source_three_turns}"  )
+#    print(f"Target only Turn 1: {p_target_no_source_first_turn}\n "
+#          f"Target only Turn 2: {p_target_no_source_second_turn}\n"
+#          f"Target only Turn 3: {p_target_no_source_third_turn}\n "
+#          f"Target First  Turn: {p_target_first_turn}\n"
+#          f"Target Second Turn: {p_target_second_turn}\n"
+#          f"Target only 3 Turns: {p_target_no_source_three_turns}"  )
 
     # Return True if probability exceeds threshold, False otherwise
     return 1 - p_target_no_source_three_turns
@@ -54,7 +53,8 @@ class Evaluation:
             source_count = sum(synergy.get_source_counts().values())
             target_count = sum(synergy.get_target_counts().values())
 
-            synergy_prob = calc_synergy_validity(source_count, target_count)        
+            synergy_prob = calc_synergy_validity(source_count, target_count)   
+            target_prob  =      
 
             print(f"{synergy_name:<15}: {source_count} / {target_count} -> {synergy_prob} ")
 

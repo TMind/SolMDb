63f4944a9fcba342ccb434c0372986678c899fb4
TMind <stefan2581@gmail.com>
2023-05-02 09:39:47 +0200

    Unstable Eval

--- Evaluation.py
+++ Evaluation.py
@@ -1,5 +1,5 @@
 import math
-from re import S
+from itertools import islice
 from scipy.stats import hypergeom
 from Synergy import SynergyTemplate
 
@@ -22,45 +22,66 @@ def calc_synergy_validity(synergy):
 
     p_cond_1 = {}
     p_cond_2 = {}
+    p_cond_2[0] = 0 
+    
     inverse = 1
+    result = {}
     
     if synergy.name == 'SPELL':       
         inverse = 0
-        pmf = []        
-        for j in range(5+1):            
-            p_source = hypergeom.pmf(j, 20, s, 5)
-            pmf.append(p_source)                  
+        
+        sources = [s]
+        for k in range(2):
+            pmf = []        
+            for j in range(s+1):            
+                p_source = hypergeom.pmf(k=j, M=20-5*k, n=s, N=5)
+                pmf.append(p_source)                 
+            #print(pmf)    
+            #print(f"Most probably number of sources drawn from {s}: {pmf.index(max(pmf))} ")
+            s -= pmf.index(max(pmf))             
+            sources.append(s)
+
+        #print(f"Sources drawn over three turns: {sources}")
         #for p in pmf:
         #    print(f"Probality: {pmf.index(p)} -> {p} ")
         #print(f"Final draw: {pmf.index(max(pmf))}")    
 
         for i in range(3):          
         
-            p_source = 1 - hypergeom.cdf(0, M=20-5*i, n=s, N=5)         # At least one source               
+            p_source = 1 - hypergeom.cdf(0, M=20-5*i, n=sources[i], N=5)         # At least one source               
             p_target = 1 - hypergeom.cdf(0, M=20-5*i, n=t, N=5)         # At least one target
 
             if math.isnan(p_source): p_source = 0
             if math.isnan(p_target): p_target = 0
 
-            p_cond_1[i] = p_source * p_target       # Both source and target
-            p_cond_2[i] = p_cond_1[i]               # Neither source nor target
-            #print(f"Drawing Both in Turn {i} : {p_source} * {p_target} = {p_cond_1[i]} => {p_cond_2[i]}")
+            p_cond_1[i]   = p_source * p_target       # Both source and target
+            p_cond_2[i+1] = p_cond_1[i]               # Neither source nor target
+            
+            prod_p_2 = math.prod([(1 - p) for p in list(islice(p_cond_2,0,i))])     
+
+            print(f"Drawing Both in Turn {i} : {p_source} * {p_target} = {p_cond_1[i]} | {p_cond_2[i]} => {prod_p_2}")
             
     else:
 
-        # Calculate probability of meeting condition in a single turn
+        #p_cond_2[0] = 0# Calculate probability of meeting condition in a single turn
         for i in range(3):
             p_cond_1[i] = calc_prob(s, t, 20-5*i, 1, 0, 5)                  # No source             
-            p_cond_2[i] = 1 - hypergeom.pmf(0, M=20-5*i, n=t, N=5)        # At least one target                
+            p_cond_2[i+1] = 1 - hypergeom.pmf(0, M=20-5*i, n=t, N=5)        # At least one target                
             if math.isnan(p_cond_1[i]): p_cond_1[i] = 0
-            if math.isnan(p_cond_2[i]): p_cond_2[i] = 0
-           # print(f"Target only Turn {i}: {p_cond_1[i]}\n"                  
-           #       f"Target Turn {i}: {p_cond_2[i]}\n"     )              
+            if math.isnan(p_cond_2[i+1]): p_cond_2[i+1] = 0
 
+                       
+    for i in range(3):
+        print(p for p in list(islice(p_cond_2,i)))
+        prod_p_cond_2 = math.prod([(1 - p) for p in list(islice(p_cond_2,i))])
+        result[i] = prod_p_cond_2 * p_cond_1[i]
+        print(f"Target only/general {i}: {p_cond_1[i]} * {p_cond_2[i]} = {prod_p_cond_2} -> Result {result[i]}") 
 
-    p_cond_123 = (                                            p_cond_1[0] + 
-                    (1 - p_cond_2[0]) *                       p_cond_1[1] + 
-                    (1 - p_cond_2[0]) * (1 - p_cond_2[1]) *   p_cond_1[2]   )
+    #p_cond_123 = sum([s for s in list(islice(result,0,2))])
+    p_cond_123 = sum([result[i] for i in range(3)])
+    #p_cond_123 = (                                            p_cond_1[0] + 
+    #                (1 - p_cond_2[1]) *                       p_cond_1[1] + 
+    #                (1 - p_cond_2[1]) * (1 - p_cond_2[2]) *   p_cond_1[2]   )
 
     #print( f"Synergy validity: {1 - p_cond_123 if inverse else p_cond_123}")
     return 1 - p_cond_123 if inverse else p_cond_123
@@ -76,12 +97,14 @@ class Evaluation:
         source_count = sum(synergy.get_source_counts().values())
         target_count = sum(synergy.get_target_counts().values())
        
-        synergy_prob = calc_synergy_validity(synergy)           
-        target_prob  = 1 - hypergeom.cdf(k=0, M=20, n=target_count, N=5)
+        synergy_prob = calc_synergy_validity(synergy)  
+        target_prob = 0
+        if synergy_prob > 0.5 :         
+            target_prob  = 1 - hypergeom.cdf(k=0, M=20, n=target_count, N=10)
               
         total_prob = synergy_prob * target_prob * 100     
         
-        print(f"{synergy.name:<15}: {source_count} / {target_count} -> {synergy_prob:.2f} => {target_prob:.2f} => {total_prob:.2f} ")
+        print(f"{synergy.name:<15}: {source_count} / {target_count} -> Validity: {synergy_prob:.2f} => Target: {target_prob:.2f} => {total_prob:.2f} ")
         return  total_prob
     
 
@@ -108,7 +131,7 @@ class Evaluation:
         print(f"Missing percentage of sources : {percentage_sources * 100 :.2f}%")
         print(f"Missing percentage of targets:  {percentage_targets * 100 :.2f}%")
 
-        synergy_total *= (1 -  percentage_targets) * (1 - percentage_sources)
+        #synergy_total *= (1 -  percentage_targets) * (1 - percentage_sources)
 
         print(f"Total synergy = {synergy_total}[{len(deck.synergy_collection.synergies.values())}] ({synergy_total / len(deck.synergy_collection.synergies.values())})")
 

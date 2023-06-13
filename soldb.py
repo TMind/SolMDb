48a22451124fb47aa9f0cfaa538172d122372270
Stefan R <stefan2581@gmail.com>
2023-06-13 13:28:36 +0200

    Store EvaluatedGraphs

--- soldb.py
+++ soldb.py
@@ -8,24 +8,20 @@ import json
 import os
 import pickle
 
-def load_deck_library(deck_library_pickle_file):
+def load_object_from_pickle(pickle_file):
+    obj = None
+    if os.path.exists(pickle_file):
+        # Load the object from the pickle file
+        with open(pickle_file, "rb") as file:
+            obj = pickle.load(file)
+        print(f"Object loaded from pickle file: {pickle_file}")
+    return obj
 
-    deck_library = None 
-    if os.path.exists(deck_library_pickle_file):
-        # Load the DeckLibrary from the pickle file
-        with open(deck_library_pickle_file, "rb") as file:
-            deck_library = pickle.load(file)
-        print("DeckLibrary loaded from pickle file.")
-    
-    return deck_library
-
-
-def save_deck_library(deck_library_pickle_file, deck_library):
-
-    # Save the DeckLibrary to the pickle file
-    with open(deck_library_pickle_file, "wb") as file:
-        pickle.dump(deck_library, file)
-    print("DeckLibrary saved to pickle file.")
+def save_object_to_pickle(pickle_file, obj):
+    # Save the object to the pickle file
+    with open(pickle_file, "wb") as file:
+        pickle.dump(obj, file)
+    print(f"Object saved to pickle file: {pickle_file}")
 
 
 def main(args):
@@ -34,13 +30,13 @@ def main(args):
 
     # Load DeckCollection from JSON file
     print(f"Loading DeckCollection from pickle: data/deck_library.pkl")
-    DeckCollection = load_deck_library("data/deck_library.pkl")
+    DeckCollection = load_object_from_pickle("data/deck_library.pkl")
     
     if DeckCollection == None:    
 
         decks = []
         if not args.username:
-            # Read entities from CSV and create universal card library        
+            # Rad entities from CSV and create universal card library        
             decks, incompletes = myUCL.load_decks_from_file(f"data/{args.filename}.json")
 
         else:
@@ -50,11 +46,10 @@ def main(args):
                 type=args.type,
                 username=args.username,
                 filename=args.filename
-            )        
-        DeckCollection = DeckLibrary(decks)
-        save_deck_library("data/deck_library.pkl", DeckCollection)
+            )  
 
-    EvaluatedGraphs = {}    
+        DeckCollection = DeckLibrary(decks)
+        save_object_to_pickle("data/deck_library.pkl", DeckCollection)
     
     if args.eval:
 
@@ -62,19 +57,23 @@ def main(args):
         if args.eval is not True:
             filename = args.eval
 
-        for fusion in DeckCollection.fusions:        
-            DeckGraph = Graph.create_deck_graph(fusion)
-            ev.evaluate_graph(DeckGraph)
-            EvaluatedGraphs[DeckGraph.graph['name']] = DeckGraph        
-            Graph.print_graph(DeckGraph,filename)
-            
+        EvaluatedGraphs = load_object_from_pickle("data/eval_graphs.pkl")
+
+        if EvaluatedGraphs == None:
+            EvaluatedGraphs = {}
+
+            for fusion in DeckCollection.fusions:        
+                DeckGraph = Graph.create_deck_graph(fusion)
+                ev.evaluate_graph(DeckGraph)
+                EvaluatedGraphs[DeckGraph.graph['name']] = DeckGraph      
+
+            save_object_to_pickle("data/eval_graphs.pkl", EvaluatedGraphs)  
+                                
+        for EGraph in EvaluatedGraphs.values():    
+            Graph.print_graph(EGraph,filename)
+                
             if args.graph:
                 Graph.write_gexf_file(DeckGraph, fusion.name.replace('|', '_'))
-
-        # Export DeckCollection to JSON
-        print(f"Exporting DeckCollection to JSON file: deck_collection.json")
-        with open("data/deck_collection.json", "w") as file:
-            json.dump(DeckCollection.to_json(), file)
 
         
         if filename: 
@@ -84,8 +83,6 @@ def main(args):
 
         if args.select_pairs:
             ev.find_best_pairs(EvaluatedGraphs)
-
-    #TODO: EvaluatedGraphs 
 
 
 if __name__ == "__main__":

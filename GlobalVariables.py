24b1d35df747d435863ba82a58bdc76f77ff7436
TMind1 <stefan2581@gmail.com>
2024-05-22 10:08:59 +0000

    Update GlobalVariables.py

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,6 +1,5 @@
 username = 'enterUsernameHere'
-#uri = "mongodb://localhost:27017"
+uri = "mongodb://localhost:27017"
 #uri = "mongodb+srv://solDB:uHrpfYD1TXVzf3DR@soldb.fkq8rio.mongodb.net/?retryWrites=true&w=majority&appName=SolDB"
-uri = "mongodb+srv://solDB:uHrpfYD1TXVzf3DR@soldb.fkq8rio.mongodb.net/?retryWrites=true&w=majority&appName=SolDB"
 myDB = None
 commonDB = None

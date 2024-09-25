1ce7d341d4cbba333a179351e56c0934cd5eba18
TMind <stefan2581@gmail.com>
2024-09-25 16:59:49 +0200

    Update gsheets.py

--- gsheets.py
+++ gsheets.py
@@ -1,31 +1,41 @@
-import gspread
-from google.oauth2.service_account import Credentials
+import os  
+import gspread  
+from google.auth import default  
+from google.oauth2.service_account import Credentials  
+  
+class GoogleSheetsClient:  
+    def __init__(self, service_account_file_path='~/soldb-gc-key.json'):          
+        # The sheet URL  
+        self.sheet_url = 'https://docs.google.com/spreadsheets/d/1HFDXfrO4uE70-HyNAxdHuCVlt_ALjBK9f6tpveRudZY/edit'  
+        # Path to service account file  
+        self.service_account_file_path = os.path.expanduser(service_account_file_path)         
+        self.gc = None  
+        self.credentials = None  
+  
+    def authenticate_google_sheets(self):  
+        """  
+        Authenticates using the service account file if it exists,  
+        otherwise uses the default service account provided by the Google Cloud environment.  
+        """  
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']  # Read-only scope for Sheets API  
+        if self.service_account_file_path and os.path.exists(self.service_account_file_path):  
+            # Authenticate with service account file  
+            self.credentials = Credentials.from_service_account_file(self.service_account_file_path, scopes=SCOPES)  
+        else:  
+            print(f"Service Account File does not exist!")
+            return
+      
+        self.gc = gspread.authorize(self.credentials)  
 
-class GoogleSheetsClient:
-    def __init__(self):
-        # Hardcode the path to your service account file and the sheet URL
-        self.service_account_file = '/Users/tmind/Documents/Code/GitHub/SolDB/binder/soldb-434422-cf0ec4d74ebd.json'
-        self.sheet_url = 'https://docs.google.com/spreadsheets/d/1HFDXfrO4uE70-HyNAxdHuCVlt_ALjBK9f6tpveRudZY/edit'
-        self.gc = None
-        self.credentials = None
-
-    def authenticate_google_sheets(self):
-        """
-        Authenticates using the hardcoded Google service account JSON and authorizes access to Google Sheets.
-        """
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-        self.credentials = Credentials.from_service_account_file(self.service_account_file, scopes=SCOPES)
-        self.gc = gspread.authorize(self.credentials)
-
-    def read_data_from_google_sheet(self, worksheet_name):
-        """
-        Reads data from the fixed Google Sheet and worksheet.
-        """
-        # Authenticate and open the sheet by URL
-        if not self.gc:
-            self.authenticate_google_sheets()
-        
-        sh = self.gc.open_by_url(self.sheet_url)
-        worksheet = sh.worksheet(worksheet_name)
-        rows = worksheet.get_all_values()
-        return rows
+    def read_data_from_google_sheet(self, worksheet_name):  
+        """  
+        Reads data from the specified Google Sheet and worksheet.  
+        """  
+        # Authenticate and open the sheet by URL  
+        if not self.gc:  
+            self.authenticate_google_sheets()  
+          
+        sh = self.gc.open_by_url(self.sheet_url)  
+        worksheet = sh.worksheet(worksheet_name)  
+        rows = worksheet.get_all_values()  
+        return rows  

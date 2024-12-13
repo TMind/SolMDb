e1f68b3388d36934828974e6790e06bbc98377ac
TMind <stefan2581@gmail.com>
2024-12-13 23:12:15 +0100

    CHG: Debug Level INFO

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -9,7 +9,7 @@ from CustomCss import CSSManager
 from GSheetsClient import GoogleSheetsClient
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[
                         logging.FileHandler("app.log"),
